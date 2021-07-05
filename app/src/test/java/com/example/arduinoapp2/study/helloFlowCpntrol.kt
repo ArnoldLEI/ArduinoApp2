525982e5097bbba39ecd6f671eb17a596c005b14
ArnoldLei <ae865566@gmail.com>
2021-07-05 15:29:52 +0800

    123

--- app/src/test/java/com/example/arduinoapp2/study/helloFlowCpntrol.kt
+++ app/src/test/java/com/example/arduinoapp2/study/helloFlowCpntrol.kt
@@ -38,4 +38,23 @@ fun main() {
     println(level2)
 
 
+    //for
+    for (i in 1..10){
+        print("$i ")
+    }
+    println()
+    for (i in 1..10 step 2){
+        print("$i ")
+    }
+    println()
+    for (i in 1 until 10){
+        print("$i ")
+    }
+    println()
+    for (i in 10 downTo 1){
+        print("$i ")
+    }
+    println()
+
+
 }
