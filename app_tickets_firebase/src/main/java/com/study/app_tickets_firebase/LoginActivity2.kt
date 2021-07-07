d9980182076327b2f00d6f0c34f0a060712dd018
ArnoldLei <ae865566@gmail.com>
2021-07-07 15:46:38 +0800

    123

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/LoginActivity2.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/LoginActivity2.kt
@@ -1,11 +1,31 @@
 package com.study.app_tickets_firebase
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login2.*
 
 class LoginActivity2 : AppCompatActivity() {
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        context = this
+        title = "雲端購票 - 登入"
     }
+
+    fun login(view: View){
+        val userName = et_username.text.toString()
+        val intent = Intent(context, MainActivity::class.java)
+        intent.putExtra("userName", userName)
+        startActivity(intent)
+    }
+
+    fun consoleLogin(view: View){
+
+    }
+
+
 }
