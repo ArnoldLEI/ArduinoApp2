15d5314b6cb10dac38582be48120c090869f35dc
ArnoldLei <ae865566@gmail.com>
2021-07-08 16:55:35 +0800

    123

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/OrderListActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/OrderListActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -17,7 +18,7 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_order_list.*
 import java.lang.Exception
 
-class OrderListActivity : AppCompatActivity() {
+class OrderListActivity : AppCompatActivity(), RecyclerViewAdapter.RowOnItemClickListener{
 
     val database = Firebase.database
     val myRef = database.getReference("ticketsStock")
@@ -70,11 +71,26 @@ class OrderListActivity : AppCompatActivity() {
         // init recycler view
         recycler_view.apply {
             layoutManager = LinearLayoutManager(context)
-            recyclerViewAdapter = RecyclerViewAdapter()
+            recyclerViewAdapter = RecyclerViewAdapter(this@OrderListActivity)
             adapter = recyclerViewAdapter
             // 分隔線
             val divider = DividerItemDecoration(context, StaggeredGridLayoutManager.VERTICAL)
             addItemDecoration(divider)
         }
+    }
+    override fun onItemClickListener(order: Order) {
+        val key = order.key
+        val alert = AlertDialog.Builder(context)
+        alert.setTitle("退票")
+        alert.setMessage("$key 是否要退票 ?")
+        alert.setPositiveButton("是") { diglog, which ->
+            myRef.child("orders/" + userName + "/" + key).removeValue()
+            val returnTickets = TicketsStock.totalAmount + order.allTickets
+            myRef.child("totalAmount").setValue(returnTickets)
+        }
+        alert.setNegativeButton("否",null)
+        alert.show()
+
+        //Toast.makeText(context, order.toString(), Toast.LENGTH_SHORT).show()
     }
 }
